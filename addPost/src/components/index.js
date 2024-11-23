00357e6bc22e4c770b5b477f7942df663766c86e
JunaidKhan19 <khanjunaid1719@gmail.com>
2024-11-23 15:37:02 +0530

    added more components to the project.

--- addPost/src/components/index.js
+++ addPost/src/components/index.js
@@ -1,7 +1,23 @@
 import Header from "./header/header";
+import LogoutBtn from "./header/LogoutBtn";
 import Footer from "./footer/footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import Select from "./Select"
+import RTE from "./RTE"
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select,
+    RTE
 }
